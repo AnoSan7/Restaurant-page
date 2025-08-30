28ec3a1d7f9772ed19d9fd989d00e3ddb5d87987
AnoSan <anomitra.santra.mat24@itbhu.ac.in>
2025-08-31 04:57:11 +0530

    Change focus design

--- src/script.js
+++ src/script.js
@@ -9,16 +9,25 @@ const homeButton = document.getElementById('homeButton');
 const menuButton = document.getElementById('menuButton');
 const aboutButton = document.getElementById('aboutButton');
 
+const arr=[homeButton, menuButton, aboutButton];
+
 document.addEventListener('DOMContentLoaded', () => {
     loadHome();
+    homeButton.classList.add('focused');
     homeButton.focus();
 });
 homeButton.addEventListener('click', () => {
+    arr.forEach(button => button.classList.remove('focused'));
+    homeButton.classList.add('focused');
     loadHome();
 });
 menuButton.addEventListener('click', () => {
+    arr.forEach(button => button.classList.remove('focused'));
+    menuButton.classList.add('focused');
     loadMenu();
 });
 aboutButton.addEventListener('click', () => {
+    arr.forEach(button => button.classList.remove('focused'));
+    aboutButton.classList.add('focused');
     loadAbout();
 });
